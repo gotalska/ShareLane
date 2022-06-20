81de4114eb53f30a4d964ba7dbec54215b9bad57
Olga GOtalskaia <gotalska@tut.by>
2022-06-20 19:40:26 +0300

    New branch: discount tests

--- src/main/java/org/example/Discount.java
+++ src/main/java/org/example/Discount.java
@@ -9,31 +9,41 @@ import org.testng.annotations.Test;
 public class Discount {
     @Test
     public void logIn() {
-            System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
-            WebDriver driver = new ChromeDriver();
-            driver.get("https://www.sharelane.com/cgi-bin/register.py");
-            WebElement zipCodeField = driver.findElement(By.name("zip_code"));
-            zipCodeField.sendKeys("12345");
-            driver.findElement(By.cssSelector("input[value='Continue']")).click();
-            WebElement firstNameField = driver.findElement(By.name("first_name"));
-            firstNameField.sendKeys("John");
-            WebElement lastNameField = driver.findElement(By.name("last_name"));
-            lastNameField.sendKeys("Dow");
-            WebElement email = driver.findElement(By.name("email"));
-            email.sendKeys("test@testestest.test");
-            WebElement password = driver.findElement(By.name("password1"));
-            password.sendKeys("John123");
-            WebElement confirmPassword = driver.findElement(By.name("password2"));
-            confirmPassword.sendKeys("John123");
-            driver.findElement(By.cssSelector("input[value='Register']")).click();
-            WebElement accountCreated = driver.findElement(By.cssSelector("span.confirmation_message"));
-            accountCreated.isDisplayed();
-            String Email = driver.findElement(By.xpath("//td[text()='Email']/following::b")).getText();
-            String Password = driver.findElement(By.xpath("//td[text()='Password']/following::td")).getText();
-            driver.findElement(By.cssSelector("img[src='../images/logo.jpg']")).click();
-            driver.findElement(By.name("email")).sendKeys(Email);
-            driver.findElement(By.name("password")).sendKeys(Password);
-            driver.findElement(By.cssSelector("input[value='Login']")).click();
-        }
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.sharelane.com/cgi-bin/register.py");
+        WebElement zipCodeField = driver.findElement(By.name("zip_code"));
+        zipCodeField.sendKeys("12345");
+        driver.findElement(By.cssSelector("input[value='Continue']")).click();
+        WebElement firstNameField = driver.findElement(By.name("first_name"));
+        firstNameField.sendKeys("John");
+        WebElement lastNameField = driver.findElement(By.name("last_name"));
+        lastNameField.sendKeys("Dow");
+        WebElement email = driver.findElement(By.name("email"));
+        email.sendKeys("test@testestest.test");
+        WebElement password = driver.findElement(By.name("password1"));
+        password.sendKeys("John123");
+        WebElement confirmPassword = driver.findElement(By.name("password2"));
+        confirmPassword.sendKeys("John123");
+        driver.findElement(By.cssSelector("input[value='Register']")).click();
+        WebElement accountCreated = driver.findElement(By.cssSelector("span.confirmation_message"));
+        accountCreated.isDisplayed();
+        String Email = driver.findElement(By.xpath("//td[text()='Email']/following::b")).getText();
+        String Password = driver.findElement(By.xpath("//td[text()='Password']/following::td")).getText();
+        driver.findElement(By.cssSelector("img[src='../images/logo.jpg']")).click();
+        driver.findElement(By.name("email")).sendKeys(Email);
+        driver.findElement(By.name("password")).sendKeys(Password);
+        driver.findElement(By.cssSelector("input[value='Login']")).click();
     }
+
+    @Test
+    public void discountLessThen20Books() {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        logIn();
+        driver.findElement(By.xpath("/html/body/center/table/tbody/tr[5]/td/table/tbody/tr/td[1]/table/tbody/tr[3]/td/a")).click();
+
+    }
+
+}
 
